9876b06659504482e188688eeaeef81e18707a49
pintofelipe <afpintou@ufpso.edu.co>
2025-10-20 23:22:34 -0500

    feat: add Exercise NEW

--- codeforce/71A.cpp
+++ codeforce/71A.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
 
-int main(){
+using namespace std;
 
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int n;
+    string s;
+    cin>>n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin>>s;
+
+        if((int)s.size() > 10){
+            cout << s.front() << (s.size()-2)<<s.back()<<'\n';
+        }else{
+            cout<<s<<'\n';
+        }
+    }
     
 
 
