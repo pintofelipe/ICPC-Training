760b34a3b620034431acaa0636dff8c573747047
pintofelipe <afpintou@ufpso.edu.co>
2025-12-25 18:51:41 -0500

    doc: add new doc the hugo

---
+++ books/AnttiLaaksonen/macros.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+#ifdef LOCAL
+#define debug(x) cout << #x << " = " << x << '\n'
+#else
+#define debug(x)
+#endif
+
+
+
+int main (){
+
+
+
+
+
+
+    return 0;
+}
---
+++ books/HugoHumbertoMorales/Chapter1/Series.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //Sumatorias
+
+    // (n(n+1))/2  for n >= n
+
+    //Paso inductivo n = K^2,
+
+    //Paso Pos-Inductivo n= (k+1)^2
+
+
+    //Paso inductivo 1^3 + 2^3 + 3^3 +...+ k^3 n = k,
+
+    //(k^2(k+1)^2)/4
+
+
+    //Paso pos-inductivo
+    // 1^3 + 2^3 + 3^3 + (k+1)^3
+    // ((k+1)^2*(k+2)^2)/4
+
+
+
+    //Serie geometrica
+    //a, a*r, a * r^2, a*r^3, ..., a*r^n
+
+    return 0;
+}
